3473eed1198008f3fc61de1eb4d2f75877df8bb7
Philippe Bolduc <pbolduc2354@gmail.com>
2018-06-10 17:01:52 +0000

    can read data

--- https_example.js
+++ https_example.js
@@ -3,14 +3,18 @@ var https = require("https");
 console.log("i did it!");
 
 var options = {
-  host: 'www.example.org',
+  host: 'www.google.com',
   path: '/'
 };
 
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
